ac6fec19843a03c8ff626393aed30435165ec1ce
Jack Neilson <jackdneilson@gmail.com>
2018-02-05 12:28:37 +0000

    Lab 1

--- labs/week1/inc/scripts/header.php
+++ labs/week1/inc/scripts/header.php
@@ -6,17 +6,18 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>The Blog of Charles Babbage</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
 <header>
     <h1>The Blog of Charles Babbage</h1>
     <nav>
         <ul>
-            <li><a href="./">Home Page</a></li>
-            <li><a href="blog">My Blog</a></li>
-            <li><a href="about">About Me</a></li>
-            <li><a href="contactus">Contact Me</a></li>
-            <?
+            <li><a href="../">Home Page</a></li>
+            <li><a href="../../index.php">My Blog</a></li>
+            <li><a href="../../index.php">About Me</a></li>
+            <li><a href="../../index.php">Contact Me</a></li>
+            <?php
             if (isset($_SESSION['username'])) {
                 echo "<li><a href='createarticle'>Create Article</a></li>";
                 echo "<li><a href='logout'>Logout</a></li>";
